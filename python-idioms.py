91ebbb6475eaa3adb756cadb5a0b66ee54d0f6cc
Dillon Allan <dillonlallan@gmail.com>
2020-02-07 08:36:23 -0700

    Initial commit

---
+++ python-idioms.py
@@ -0,0 +1,24 @@
+# slices
+
+# chained operators e.g. x < y < z
+
+# unpacking
+    # ignore variable '_'
+    # use for swapping values w/o creating a temp
+
+# the 'in' keyword: sets vs. lists
+
+# comprehensions and generators
+    # filter with clauses
+
+# 'is' vs '=='
+
+# 'with' keyword for better IO
+
+# line continuations
+
+# string formatting (% vs .format vs f strings)
+
+# Set operations
+
+# *BONUS TOPIC: type hints
